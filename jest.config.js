1c770cb889a1c59ce53f00371fc4b5cc0489f046
AlexStorm <info@alexstorm.ru>
2022-12-05 08:23:53 +0530

    pathes fixes

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../../coverage/libs/fui/icons',
+  coverageDirectory: '../../../coverage/packages/uimatrix/icons',
   transform: {
     '^.+.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
